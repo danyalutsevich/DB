40dd4fc6d544f722e1e979582c8ef1f8193cee00
danyalutsevich <luchevich31@gmail.com>
2022-05-18 11:38:23 +0300

    basics

---
+++ DB/SQL/CreateRandoms.sql
@@ -0,0 +1,15 @@
+﻿ -- DDL
+
+CREATE TABLE -- командаа создания таблицы
+randoms( -- имя таблицы
+id -- имя поля
+UNIQUEIDENTIFIER -- тип данных 
+PRIMARY KEY, -- аттрибуты поля 
+rnd -- имя 
+INT -- тип
+NOT NULL -- атрибуты
+)
+
+
+
+
---
+++ DB/SQL/InsertRandoms.sql
@@ -0,0 +1,13 @@
+﻿-- DML
+INSERT INTO -- внесение данных
+randoms		-- куда внести даные
+VALUES		-- перечень значений
+(NEWID(), -- попадает в id
+CHECKSUM(NEWID())); -- попадает в rnd
+
+INSERT INTO randoms(rnd,id) VALUES (CHECKSUM(NEWID()),NEWID()) -- указываем перечень и порядок полей при внесении
+
+DELETE FROM randoms WHERE id = '407EA299-2EE8-467D-8A90-535B7E0C2796'
+
+INSERT INTO randoms(rnd,id) VALUES (999999999,'407EA299-2EE8-467D-8A90-535B7E0C2796')
+ 
