3647725799b827e8f7249996ad27e300d80b55b5
danyalutsevich <luchevich31@gmail.com>
2022-05-27 02:09:48 +0300

    HAVING

--- DB/Company/Having.sql
+++ DB/Company/Having.sql
@@ -1,4 +1,85 @@
 ﻿
+
+-- HOMEWORK
+
+
+	-- худший по продажам (товаров-штук) за сегодня отдел
+	
+SELECT TOP 1
+	MAX(D.Name) [Name],
+	SUM(S.Cnt) [Quantity]
+FROM
+	Sales S
+	JOIN Managers M ON S.ID_manager = M.Id
+	JOIN Departments D ON M.Id_main_dep = D.Id
+WHERE
+	CAST(S.Moment AS DATE) = CAST(CURRENT_TIMESTAMP - DATEADD(YEAR,1,0) AS DATE) 
+GROUP BY 
+	D.Id
+ORDER BY 2
+
+
+-- отделы, суммы продаж за месяц по которым превышают 100000 грн
+
+SELECT 
+	MAX(D.Name) [Name],
+	SUM(S.Cnt) [Quantity],
+	SUM(S.Cnt * P.Price) [UAH]
+FROM
+	Sales S
+	JOIN Managers M ON S.ID_manager = M.Id
+	JOIN Departments D ON M.Id_main_dep = D.Id
+	JOIN Products P ON S.ID_product = P.Id
+WHERE
+	MONTH(S.Moment ) = 5 
+GROUP BY 
+	D.Id
+HAVING
+	SUM(S.Cnt * P.Price)>2000000
+ORDER BY 3
+
+
+-- отделы сумма продаж (грн) которых меньше чем в среднем за сегодня
+
+SELECT 
+	MAX(D.Name) [Name],
+	SUM(S.Cnt) [Quantity],
+	SUM(S.Cnt * P.Price) [UAH]
+FROM
+	Sales S
+	JOIN Managers M ON S.ID_manager = M.Id
+	JOIN Departments D ON M.Id_main_dep = D.Id
+	JOIN Products P ON S.ID_product = P.Id
+WHERE
+	CAST(S.Moment AS DATE) = CAST(CURRENT_TIMESTAMP - DATEADD(YEAR,1,0) AS DATE) 
+GROUP BY 
+	D.Id
+HAVING 
+	SUM(S.Cnt * P.Price)<
+	(
+		SELECT
+			AVG(AVERAGE.UAH)
+		FROM
+		(
+			SELECT 
+				SUM(S.Cnt * P.Price) [UAH]
+			FROM
+				Sales S
+				JOIN Managers M ON S.ID_manager = M.Id
+				JOIN Departments D ON M.Id_main_dep = D.Id
+				JOIN Products P ON S.ID_product = P.Id
+			WHERE
+				CAST(S.Moment AS DATE) = CAST(CURRENT_TIMESTAMP - DATEADD(YEAR,1,0) AS DATE) 
+			GROUP BY 
+				D.Id
+			
+		) AVERAGE 
+	) 
+
+ORDER BY 3
+
+
+
 SELECT 
 	P.Name [Name],
 	SUM(S.Cnt) [Quantity]
@@ -91,6 +172,7 @@ GROUP BY
 	M.Id
 ORDER BY 
 	2 DESC
+
 
 
 
