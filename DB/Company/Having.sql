aaf9e37978222df2476f884de0856015f4f02a3f
danyalutsevich <luchevich31@gmail.com>
2022-05-29 15:26:47 +0300

    NOT IN

--- DB/Company/Having.sql
+++ DB/Company/Having.sql
@@ -2,6 +2,31 @@
 
 -- HOMEWORK
 
+-- сотрудники, которые не продавали гвозди за сегодня
+
+SELECT 
+	CONCAT (M.Surname, ' ' , M.Name) [Managers] 
+FROM
+	Managers M
+WHERE
+	M.Id NOT IN ( -- выбираем оставшихся сотрудников и получаем тех кто не продавал гвозди
+	
+		SELECT    -- получаем всех сотрудников которые продавали гвозди за сегодня
+			MIN(M.Id) [Manager Id]
+		FROM
+			Sales S
+			JOIN Managers M ON S.Id_manager = M.Id
+		WHERE
+			CAST(S.Moment AS DATE) = CAST(CURRENT_TIMESTAMP - DATEADD(YEAR,1,0) AS DATE) 
+			AND (S.ID_product = 'B6D20749-B495-4B1A-BA1C-80B88E78B7CD' OR S.ID_product = 'DA1E17BB-A90D-4C79-B801-5462FB070F57')
+		GROUP BY
+			S.ID_manager
+	) 
+
+	
+
+
+-- END OF HOMEWORK
 
 	-- худший по продажам (товаров-штук) за сегодня отдел
 	
