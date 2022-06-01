fb1cc0941712fbecd28efbf5e49c938d28df7ea2
danyalutsevich <luchevich31@gmail.com>
2022-06-01 19:37:54 +0300

    Func3

---
+++ DB/Company/Func3.sql
@@ -0,0 +1,28 @@
+﻿
+CREATE OR ALTER FUNCTION
+	FindProdByName(@fragment NVARCHAR(30))
+RETURNS TABLE
+AS
+	RETURN
+		SELECT *
+		FROM Products P
+		WHERE P.Name LIKE '%' + @fragment + '%'
+
+
+SELECT * FROM dbo.FindProdByName(N'гвозд')
+
+
+SELECT 
+	MIN(FP.Name) [Product],
+	SUM(S.Cnt) [Count]
+FROM Sales S
+	JOIN dbo.FindProdByName(N'гвозд') FP ON S.ID_product = FP.Id
+WHERE
+	MONTH(S.Moment)= 4
+GROUP BY S.ID_product
+
+
+
+
+
+
